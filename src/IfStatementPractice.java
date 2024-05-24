bcfa42e0f04249f626b3cf749798a009c5de3fc6
soncu <salim.oncu@virgosol.com>
2024-05-24 09:42:11 +0300

    first commit

---
+++ src/IfStatementPractice.java
@@ -0,0 +1,13 @@
+
+public class IfStatementPractice {
+
+    public static void main(String[] args) {
+
+      int x= 5;
+      Integer y = new Integer(x);
+
+      int z = y.intValue();
+        System.out.println(z);
+    }
+}
+
